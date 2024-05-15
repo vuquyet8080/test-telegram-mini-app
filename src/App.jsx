8eb33df31f7ac3f3fedd905d6b6bcce492fdf661
vuquyet808 <vuquyet808@gmail.com>
2024-05-15 15:18:12 +0700

    fix color

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import WebApp from "@twa-dev/sdk";
+console.log("xxx", WebApp.version);
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="bg-white">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -39,7 +40,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
